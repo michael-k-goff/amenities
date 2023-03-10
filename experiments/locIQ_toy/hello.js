4a0977c0b98eda5dfe06e09f838d25966b4edc0f
Camilo Schaser-Hughes <camilo.schaserhughes@gmail.com>
2023-03-09 19:30:22 -0800

    got a park result

--- experiments/locIQ_toy/hello.js
+++ experiments/locIQ_toy/hello.js
@@ -1,3 +1,18 @@
-let my_var = "Hello Wrodl!";
+// import axios from "axios";
 
-console.log(my_var);
+const axios = require("axios");
+
+let park_url =
+  "https://us1.locationiq.com/v1/nearby?key=APIKEYHERE&lat=45.563730&lon=-122.662930&tag=park&radius=1600&format=json";
+
+axios.get(park_url).then((response) => {
+  results = response.data.map((location) => {
+    let name = location.name;
+    name += " ".repeat(30 - name.length);
+    return `${name}lat:${location.lat}\tlon:${location.lon}`;
+  });
+
+  results.forEach((res) => {
+    console.log(res);
+  });
+});
